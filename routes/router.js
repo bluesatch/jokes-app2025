0739a2fa8d1f441f78c95b415e6f196161a30e21
bluesatch <satchwerk@gmail.com>
2025-10-27 15:16:58 -0500

    working on allJokes.ejs

--- routes/router.js
+++ routes/router.js
@@ -1,13 +1,26 @@
 const express = require('express')
 const router = express.Router()
+const axios = require('axios')
+const getJoke = require('../helpers/getJoke')
 
 // http://localhost:3001 => home page
 router.get('/', (req, res)=> {
     // res.send('working...') => display home page
-    res.render('pages/home', {
-        title: "Satch's Jokes App",
-        name: "satch's jokes app!"
-    })
+    const url = `https://api.sampleapis.com/jokes/goodJokes`
+
+    axios.get(url)
+        .then(resp => {
+            // console.log(resp.data)
+            // move to helper...
+            // const randomJoke = resp.data[Math.floor(Math.random() * resp.data.length)]
+
+            res.render('pages/home', {
+                title: "Satch's Jokes App",
+                name: "satch's jokes app!",
+                joke: getJoke(resp.data)
+            })
+        })
+
 })
 
 router.use('/jokes', require('./api/jokesRoutes'))
@@ -17,7 +30,7 @@ router.use((req, res, next)=> {
     res.status(404)
     .render('pages/404', {
         title: '404 Error',
-        name: '404 Error. This page does not exist...'
+        name: 'Jokes on you. This page doesn\'t exist'
     })
 })
 
